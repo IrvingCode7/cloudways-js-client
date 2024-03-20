7ccc9686a1e9d644a368595ae334e1d0a63c8ffe
code-gio <138194389+code-gio@users.noreply.github.com>
2024-03-20 05:35:33 -0600

    set up proper buid

--- services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Lists";
-export * from "./core";
-export * from "./projects";
-
-export * from "./Lists/types";
-export * from "./core/types";
-export * from "./projects/types";
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./services/Lists";
+export * from "./services/core";
+export * from "./services/projects";
+
+export * from "./services/Lists/types";
+export * from "./services/core/types";
+export * from "./services/projects/types";
